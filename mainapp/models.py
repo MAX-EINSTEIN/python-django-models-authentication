7633f55b1c712f5c14e9064453f191e99bcc8de7
Junaid Siddiqui <maxeinstein.dev@gmail.com>
2021-04-25 14:51:57 +0530

    Module 4 done - 1 Test fails (most likely a bug in the test)

--- mainapp/models.py
+++ mainapp/models.py
@@ -2,15 +2,33 @@ from django.db import models
 from django.conf import settings
 from django.urls import reverse
 
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def clean(self) -> None:
+        self.name = self.name.lower()
+
+    def __str__(self) -> str:
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('tag_posts', args=[str(self.name)])
+    
+
+
 class BlogPost(models.Model):
     title = models.CharField(max_length=200, unique=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='posts',
                                on_delete=models.CASCADE)
     body = models.TextField()
     postdate = models.DateTimeField(auto_now_add=True, blank=True)
+    tags = models.ManyToManyField(Tag, related_name='posts')
 
     def __str__(self) -> str:
         return self.title
 
     def get_absolute_url(self):
         return reverse('post', args=[str(self.id)])
+
+
--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('tag/<str:name>', views.tag_posts, name='tag_posts'),
     path('post/<int:id>', views.post, name='post'),
 #    path('user/<int:id>', views.user_posts, name='user_posts'),
 #    path('tag/<str:name>', views.tag_posts, name='tag_posts'),
